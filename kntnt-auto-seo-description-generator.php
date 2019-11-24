2375bd5faa4f3b4241d9d846b66ff9f888a00831
Thomas Barregren <thomas@kntnt.com>
2019-11-24 10:41:16 +0100

    Simplified and fixed bug.

--- kntnt-auto-seo-description-generator.php
+++ kntnt-auto-seo-description-generator.php
@@ -6,7 +6,7 @@
  * Plugin URI:        https://github.com/Kntnt/kntnt-auto-seo-description-generator
  * GitHub Plugin URI: https://github.com/Kntnt/kntnt-auto-seo-description-generator
  * Description:       Provides the excerpt as the value of the field _genesis_description, if missing.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
  * License:           GPL-3.0+
@@ -15,32 +15,22 @@
 
 defined( 'WPINC' ) || die;
 
-add_filter( 'get_post_metadata', function ( $value, $post_id, $meta_key, $single ) {
+add_filter( 'get_post_metadata', function ( $meta_value, $post_id, $meta_key, $single ) {
 
-    // Do nothing if _genesis_description isn't requested.
-    if ( '' != $meta_key && '_genesis_description' != $meta_key ) {
-        return $value;
-    }
+    // Flag signaling whether or not this filter should be applied.
+    static $do_filter = true;
 
-    // Don't filter when this filter is called indirectly by itself.
-    static $dont_filter = false;
-    if ( $dont_filter ) {
-        return $value;
-    }
+    if ( '_genesis_description' == $meta_key && $do_filter ) {
 
-    // Get the original metadata.
-    $dont_filter = true;
-    $meta_value = get_post_meta( $post_id, $meta_key, $single );
-    $dont_filter = false;
+        // Get the the value of _genesis_description.
+        $do_filter = false;
+        $meta_value = get_post_meta( $post_id, $meta_key, $single );
 
-    // Use the excerpt if  _genesis_description is empty.
-    if ( is_array( $meta_value ) ) {
-        if ( ! isset( $meta_value['_genesis_description'] ) || '' == $meta_value['_genesis_description'][0] ) {
-            $meta_value['_genesis_description'][0] = get_the_excerpt( $post_id );
+        // Use excerpt if _genesis_description is missing or empty.
+        if ( ! $meta_value ) {
+            $meta_value = [ get_the_excerpt( $post_id ) ];
         }
-    }
-    else if ( '' == $meta_value ) {
-        $meta_value = get_the_excerpt( $post_id );
+
     }
 
     return $meta_value;
