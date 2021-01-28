6a89358f3dac19013d8722d714462b16ec47f1a7
Thomas Barregren <thomas@kntnt.com>
2021-01-28 20:45:08 +0100

    Transformed from a standard plugin to a mu-plugin.

--- kntnt-auto-seo-description-generator.php
+++ kntnt-auto-seo-description-generator.php
@@ -4,16 +4,15 @@
  * @wordpress-plugin
  * Plugin Name:       Kntnt Auto SEO Description Generator
  * Plugin URI:        https://github.com/Kntnt/kntnt-auto-seo-description-generator
- * GitHub Plugin URI: https://github.com/Kntnt/kntnt-auto-seo-description-generator
  * Description:       Provides the excerpt as the value of the field _genesis_description, if missing.
- * Version:           1.0.1
+ * Version:           2.0.0
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-defined( 'WPINC' ) || die;
+defined( 'ABSPATH' ) || die;
 
 add_filter( 'get_post_metadata', function ( $meta_value, $post_id, $meta_key, $single ) {
 
